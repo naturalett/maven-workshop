1fa234a3618d86fae0904d2e7ce4a579a039356b
lidor ettinger <lidor.ettinger@gmail.com>
2023-08-17 01:00:14 +0300

    update

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.sql.Timestamp;
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
 public class HelloWorldApplication {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @ConditionalOnProperty(name = "app.init-db", havingValue = "true", matchIfMissing = true)
+    @Bean
+    public void init() {
+        // Create the 'healthcheck' table if it doesn't exist
+        jdbcTemplate.update(
+                "CREATE TABLE IF NOT EXISTS healthcheck (id INT AUTO_INCREMENT PRIMARY KEY, date TIMESTAMP);"
+        );
+    }
 
     @GetMapping("/")
     public String hello() {
@@ -16,6 +37,8 @@ public class HelloWorldApplication {
 
     @GetMapping("/healthcheck")
     public String healthCheck() {
+        Date currentDate = new Date();
+        jdbcTemplate.update("INSERT INTO healthcheck (date) VALUES (?)", new Timestamp(currentDate.getTime()));
         return "Health check passed!";
     }
 
